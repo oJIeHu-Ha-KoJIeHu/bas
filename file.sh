b822e98490ecca36a9c31229b0119f23e5ee982d
oJIeHu-Ha-KoJIeHu <vakhranev1998@mail.ru>
2017-06-30 17:28:07 +0300

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 read Value1;
-if [ $Value1 = 0 ]; then 
+if [[ $Value1 = 0 ]]; then 
     echo "Error";
     exit 0;
 fi
@@ -9,10 +9,10 @@ read Value2;
 read Value3;
 D=$(( ($Value2)*($Value2)-4*($Value1)*($Value3) ));
 
-if [ $D = 0 ]; then
+if [[ $D = 0 ]]; then
     echo -n "x = "
     echo -e "scale=1\n-0.5*($Value2)/($Value1)" | bc
-elif [ $D -gt 0 ]; then
+elif [[ $D -gt 0 ]]; then
     echo -n "x1 = "
     echo -e "scale=1\n0.5*(-($Value2)+sqrt($D))/($Value1)" | bc
     echo -n "x2 = "
